1ad41d4ea917848c2f48794dc36421b7251968c5
Aleksander <olek130@gmail.com>
2019-12-10 21:49:05 +0100

    Getting the weather forecast through HTML request to Darksky.

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-console.log('Starting app');
-
-
-setTimeout( () => {
-        console.log('0 second timer');
-    },
-    0
-);
-
-setTimeout( () => {
-        console.log('2 second timer');
-    },
-    2000
-);
+const request = require('request');
 
 
 
+const authKey = "852cab1c66a15f0986b4f1c481adad49";
+const longitude = '17.0333300';
+const latitude = '51.1000000';
 
-console.log('Stopping app');
+let url = "https://api.darksky.net/forecast/";
+url += authKey + "/";
+url += latitude + "," + longitude;
+url += "?units=si";
+url += "&lang=en";
+
+
+console.log(url);
+
+request({ url: url, json: true }, (error, response) => {
+    const data = response.body.currently;
+    console.log(response.body.daily.data[0].summary + " It is currently " + data.temperature +
+        " degrees out. There is " + data.precipProbability + "% change of rain.");
+});
+
