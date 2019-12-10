ef7f3dc7799db8f84494be67d2c44bb307d8b950
Aleksander <olek130@gmail.com>
2019-12-10 14:18:04 +0100

    First asynchronous function call

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log('Starting app');
+
+
+setTimeout( () => {
+        console.log('0 second timer');
+    },
+    0
+);
+
+setTimeout( () => {
+        console.log('2 second timer');
+    },
+    2000
+);
+
+
+
+
+console.log('Stopping app');
