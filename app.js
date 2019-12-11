c724c556cc1f9d1b767b3a164270056ef3fa4073
Aleksander <olek130@gmail.com>
2019-12-11 11:55:46 +0100

    Simlpe error handling implemented.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const authKeys = JSON.parse(fs.readFileSync('../auth_keys.json').toString());
 const authKey_Darksky_ = authKeys.darksky;
 const authKey_Mapbox = authKeys.mapbox;
 
-const place = "Wroclaw";
+const place = "razdwatrzytest";
 let latitude = 0;
 let longitude = 0;
 
@@ -15,14 +15,19 @@ let getWeather = (place) => {
     geocoding_url += "&limit=1";
 
     request({url: geocoding_url, json: true} , (error,response) => {
-        longitude = response.body.features[0].center[0];
-        latitude = response.body.features[0].center[1];
-        console.log("Weather for: " + response.body.features[0].place_name + "( Longitude: " + longitude + (longitude >= 0 ? "E" : "W") + ", Latitude: " + latitude + (latitude >= 0 ? "N" : "S") + " )");
+        if(error) {
+            console.log("Unable to connect to geocoding service");
+        } else if(response.body.features.length === 0) {
+            console.log("Unable to find the place with the following name: " + place);
+        } else {
+            longitude = response.body.features[0].center[0];
+            latitude = response.body.features[0].center[1];
+            console.log("Weather for: " + response.body.features[0].place_name + "( Longitude: " + longitude + (longitude >= 0 ? "E" : "W") + ", Latitude: " + latitude + (latitude >= 0 ? "N" : "S") + " )");
 
-        sendWeatherRequest(longitude, latitude);
+            sendWeatherRequest(longitude, latitude);
+        }
     });
 };
-
 let sendWeatherRequest = (longitude, latitude) => {
     let url = "https://api.darksky.net/forecast/";
     url += authKey_Darksky_ + "/";
@@ -30,10 +35,18 @@ let sendWeatherRequest = (longitude, latitude) => {
     url += "?units=si";
     url += "&lang=en";
 
-    request({ url: url, json: true }, (error, response) => {
-        const data = response.body.currently;
-        console.log(response.body.daily.data[0].summary + " It is currently " + data.temperature +
-            " degrees out. There is " + data.precipProbability + "% change of rain.");
+    console.log(url);
+
+    request({ url: url, json: true }, (error, response, body) => {
+        if(error) {
+            console.log("Unable to connect to weather service");
+        } else if(response.body.error) {
+            console.log("Error in the response : (" + response.body.code + ") "+ response.body.error);
+        } else {
+            const data = response.body.currently;
+            console.log(response.body.daily.data[0].summary + " It is currently " + data.temperature +
+                " degrees out. There is " + data.precipProbability + "% change of rain.");
+        }
     });
 };
 
